fc7c7ca1b98822523e6354af20e8e2ca08d31c8c
Neil Horman <nhorman@tuxdriver.com>
2015-04-09 15:20:24 -0400

    Adding copyright info to files

--- src/freight-builder/main.c
+++ src/freight-builder/main.c
@@ -1,3 +1,24 @@
+/*********************************************************
+ *Copyright (C) 2015 Neil Horman
+ *This program is free software; you can redistribute it and\or modify
+ *it under the terms of the GNU General Public License as published 
+ *by the Free Software Foundation; either version 2 of the License,
+ *or  any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+ *
+ *File: main.c
+ *
+ *Author:Neil Horman
+ *
+ *Date: 4/9/2015
+ *
+ *Description
+ *********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
