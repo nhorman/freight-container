2b7483ae40091052279d0780b538e61289f52225
Neil Horman <nhorman@tuxdriver.com>
2015-09-29 19:41:39 -0400

    xmlrpc_client: Fix header comments
    Had double * in the comments that needed removing

--- src/freight-agent/xmlrpc_client.c
+++ src/freight-agent/xmlrpc_client.c
@@ -1,23 +1,23 @@
 /*********************************************************
- * *Copyright (C) 2015 Neil Horman
- * *This program is free software; you can redistribute it and\or modify
- * *it under the terms of the GNU General Public License as published 
- * *by the Free Software Foundation; either version 2 of the License,
- * *or  any later version.
- * *
- * *This program is distributed in the hope that it will be useful,
- * *but WITHOUT ANY WARRANTY; without even the implied warranty of
- * *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * *GNU General Public License for more details.
- * *
- * *File: xmlrpc_client.c 
- * *
- * *Author:Neil Horman
- * *
- * *Date:
- * *
- * *Description implements access to a freightproxy based db server
- * *********************************************************/
+ *Copyright (C) 2015 Neil Horman
+ *This program is free software; you can redistribute it and\or modify
+ *it under the terms of the GNU General Public License as published 
+ *by the Free Software Foundation; either version 2 of the License,
+ *or  any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+ *
+ *File: xmlrpc_client.c 
+ *
+ *Author:Neil Horman
+ *
+ *Date:
+ *
+ *Description implements access to a freightproxy based db server
+ *********************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
